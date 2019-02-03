494899d236276053faf0daad9b97425b6b9e637d
Matti Paksula <matti.paksula@iki.fi>
2019-02-03 14:44:01 +0200

    -m in bucket delete

--- lib/gbuild/clean_command.rb
+++ lib/gbuild/clean_command.rb
@@ -5,8 +5,8 @@ module Gbuild
     parameter "PROJECT", "project"
 
     def execute
-      Kommando.run "gsutil rm -r #{Gbuild.source_bucket(project)}", output: true
-      Kommando.run "gsutil rm -r #{Gbuild.source_bucket(project)}", output: true
+      Kommando.run "gsutil -m rm -r #{Gbuild.source_bucket(project)}", output: true
+      Kommando.run "gsutil -m rm -r #{Gbuild.source_bucket(project)}", output: true
     end
   end
 end
